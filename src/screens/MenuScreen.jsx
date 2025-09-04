80fb53958c404a8c2ab3bafdf952d5bf102338fb
alexastagr <contact@alexasta.gr>
2025-09-04 11:13:38 +0300

    menu screen

--- src/screens/MenuScreen.jsx
+++ src/screens/MenuScreen.jsx
@@ -1,10 +1,22 @@
+// components
 import Logo from "../components/Logo";
 import Footer from "../components/Footer";
+import Button from "../components/Button";
 
-function MenuScreen() {
+function MenuScreen({ starter }) {
+  const btnsStyle = {
+    width: 400,
+    maxWidth: "90dvw",
+    margin: "auto",
+    marginTop: 50,
+  };
   return (
     <>
-      <Logo />
+      <Logo size={140} top={200} tag={false} fs={50} />
+
+      <div style={btnsStyle}>
+        <Button text={"Let's Play"} callback={starter} />
+      </div>
       <Footer />
     </>
   );
